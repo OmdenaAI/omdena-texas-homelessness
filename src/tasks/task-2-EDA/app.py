3dd0ca805e253c28134e0750999f16636cd63693
Alex Lucchesi <84862301+lucchesia7@users.noreply.github.com>
2021-09-24 14:24:49 -0700

    Update app.py

--- src/tasks/task-2-EDA/app.py
+++ src/tasks/task-2-EDA/app.py
@@ -10,7 +10,7 @@ def wrangle_with_dt(filepath):
     # Seperates the categorical columns
     categorical_cols = df.select_dtypes('object').columns
     
-    #Creates threshold for how many times you will allow the same value to show up in multiple columns in a row.
+    #Creates threshold for how many unique values you want the column to hold.
     threshold = 50
     
     #Identify high cardinality columns
@@ -21,9 +21,11 @@ def wrangle_with_dt(filepath):
     df.drop(high_card_cols, axis=1, inplace=True)
     
     # Drop columns with a high number of NaN values
+    # Only if the length of the dataframe exceeds 100 rows. Ran into issues with smaller datasets
     if len(df) > 100:
         df = df.dropna(axis = 1, thresh = 100)
     # Fill NA values with front fill. Replaces with value ahead of it.
+    # Fill first values if they are NaN's with last value of Col in DF
     df = df.fillna(method='ffill')
     df = df.fillna(method='bfill')
     
@@ -40,7 +42,7 @@ def wrangle_without_dt(filepath):
     # Seperates the categorical columns
     categorical_cols = df.select_dtypes('object').columns
     
-    #Creates threshold for how many times you will allow the same value to show up in multiple columns in a row.
+    #Creates threshold for how many unique values you want the column to hold.
     threshold = 50
     
     #Identify high cardinality columns
@@ -48,13 +50,14 @@ def wrangle_without_dt(filepath):
                       if df[col].nunique() > threshold]
     
     # Drop high cardinality columns
-    df = df.drop(high_card_cols, axis=1)
+    df.drop(high_card_cols, axis=1, inplace=True)
     
     # Drop columns with a high number of NaN values
+    # Only if the length of the dataframe exceeds 100 rows. Ran into issues with smaller datasets
     if len(df) > 100:
         df = df.dropna(axis = 1, thresh = 100)
     # Fill NA values with front fill. Replaces with value ahead of it.
-    # Replaces values at start of data with last 
+    # Fill first values if they are NaN's with last value of Col in DF
     df = df.fillna(method='ffill')
     df = df.fillna(method='bfill')
     
