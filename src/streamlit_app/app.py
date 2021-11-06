4a162547d6e980b8fa3ff41b057f8f2f96f7279a
Sarvagya Kumar <sarvagya20kumar@gmail.com>
2021-11-06 16:29:55 +0530

    Update app.py

--- src/streamlit_app/app.py
+++ src/streamlit_app/app.py
@@ -3,10 +3,10 @@ from multipage import MultiPage
 from pages import (texas_covid, income_poverty, needs_met,
                    prison_data, return_to_homelessness, 
                    homelessness_forecast,HIC_FINAL_REPORT)
-
+from pages.CPI_homeless.main import predict
 app = MultiPage()
 
-st.title("Visualization of Homelessness in Texas")
+st.title("Combating Homelessness in Texas")
 app.add_page("Income Poverty in Texas", income_poverty.app)
 app.add_page("COVID Data in Texas", texas_covid.app)
 app.add_page("Needs Met By Outreach Teams", needs_met.app)
@@ -14,4 +14,5 @@ app.add_page("Prison Data in Texas", prison_data.app)
 app.add_page("Return to Homelessness Rates", return_to_homelessness.app)
 app.add_page("Texas Homelessness forecasts", homelessness_forecast.app)
 app.add_page("HIC_FINAL_REPORT", HIC_FINAL_REPORT.app)
+app.add_page("County based Child abuse likelihood prediction", predict)
 app.run()
